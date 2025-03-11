798c891fc352f60b7cd4176d6255b1bdccbecb45
Jhonny4e20 <pedrao.kovalski@gmail.com>
2025-03-11 15:43:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 let nome = prompt("Olá, qual o seu nome?");
 
 console.log(`Boa tarde ${nome}, seja bem vindo!`);
-//Sella
+/*Foi feito um código para dar boa tardea um nome qualquer. (Let= declarar variável/ prompt = aparece a mensagem no prompt do
+programa/console.log = coloca a mensagem no console do programa.*/
+
